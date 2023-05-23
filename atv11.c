f8f2e383b41f060d95c3f40785237593056c06f2
lucasvinisan <lucasviniciuslvsl118@gmail.com>
2023-05-23 08:30:32 -0300

    modificações

---
+++ atv11.c
@@ -0,0 +1,35 @@
+//#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 100
+
+float *gerar_vetor(int n) {
+    float *vetor;
+    int i;
+    vetor = (float *)malloc(sizeof(float) * n);
+    for (i=0;i<n;i++) {
+        float num = (rand() / (float)RAND_MAX);
+        vetor[i] = num;
+    }
+    return vetor;
+}
+
+float *gerar_vetor(int n);
+
+int main() {
+
+    MPI_Init(NULL, NULL);
+    int nprocs;
+    //MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    int rank;
+    //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    //MPI_Status status;
+    
+    time_t t;
+    srand(time(NULL)*rank);
+    
+    MPI_Finalize();
+    return 0;
+}
